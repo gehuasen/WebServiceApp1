11e1de250908a21e880830cf610283fbe357b702
gehuasen <gehuasen@outlook.com>
2016-10-20 00:42:03 +0800

    添加项目文件。

---
+++ WebServiceApp1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebServiceApp1.Global" Language="C#" %>
---
+++ WebServiceApp1/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebServiceApp1.Startup))]
+namespace WebServiceApp1
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
